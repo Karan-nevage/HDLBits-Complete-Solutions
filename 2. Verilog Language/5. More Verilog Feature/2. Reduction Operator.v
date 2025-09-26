ea537e8b8b5ee42101a5f901498643d2e933e7f0
Karankumar Nevage <111281049+Karan-nevage@users.noreply.github.com>
2025-09-26 22:54:25 +0530

    Question 2

---
+++ 2. Verilog Language/5. More Verilog Feature/2. Reduction Operator.v
@@ -0,0 +1,10 @@
+module top_module (
+    input [7:0] in,
+    output parity); 
+    
+    // assign parity
+    //assign parity = in[0] ^ in[1] ^ in[2] ^ in[3] ^ in[4] ^ in[5] ^ in[6] ^ in[7];
+    
+    // you can use this also
+	  assign parity = ^in;
+endmodule
